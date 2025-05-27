9b9c07f71a792436afc69c73a7e6cdc75c8bc014
Mario <malome@jviladoms.cat>
2025-05-27 10:11:13 +0200

    Cambio url front

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,7 +18,7 @@ const port = 5000;
 
 // Configura CORS para permitir solicitudes desde el frontend
 app.use(cors({
-    origin: 'https://rinconfriki-production.up.railway.app/',
+    origin: 'merry-peace.railway.internal',
     credentials: true,
 }));
 
