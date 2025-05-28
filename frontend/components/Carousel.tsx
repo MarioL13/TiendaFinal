b29bee1d52bb9ed2fac9502defa9ca7398d3ea38
ElFanta08 <jodemo@jviladoms.cat>
2025-05-28 19:21:51 +0200

    prueba comprar cartas

--- frontend/components/Carousel.tsx
+++ frontend/components/Carousel.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Slider = () => {
   return (
-    <div className="w-full h-[80vh]">
+    <div className="w-full h-[80vh] mt-10">
       <div className="relative w-full h-[80vh] bg-slate-800 overflow-hidden">
         <input
           className="hidden peer/slider1 checkbox"
@@ -26,7 +26,7 @@ const Slider = () => {
         />
   
         <div
-          className="relative w-[300vw] h-full flex transition-all duration-500 ease-in-out peer-checked/slider1:-left-0 peer-checked/slider2:-left-[100vw] peer-checked/slider3:-left-[200vw]"
+          className="relative w-screen h-full flex transition-all duration-500 ease-in-out peer-checked/slider1:-left-0 peer-checked/slider2:-left-[100vw] peer-checked/slider3:-left-[200vw]"
         >
             <div className="relative w-full h-full flex">
             <img src="imagen_2025-05-28_191223371.jpg" className="relative w-full h-full flex object-cover object-center"/>
@@ -40,18 +40,18 @@ const Slider = () => {
         </div>
   
         <div
-          className="absolute w-full flex justify-center gap-2 bottom-12 peer-[&_label:nth-of-type(1)]/slider1:peer-checked/slider1:opacity-100 peer-[&_label:nth-of-type(1)]/slider1:peer-checked/slider1:w-10 peer-[&_label:nth-of-type(2)]/slider2:peer-checked/slider2:opacity-100 peer-[&_label:nth-of-type(2)]/slider2:peer-checked/slider2:w-10 peer-[&_label:nth-of-type(3)]/slider3:peer-checked/slider3:opacity-100 peer-[&_label:nth-of-type(3)]/slider3:peer-checked/slider3:w-10"
+          className="absolute w-full flex justify-center gap-2 bottom-12"
         >
           <label
-            className="block w-5 h-5 bg-white cursor-pointer opacity-50 z-10 transition-all duration-300 ease-in-out hover:scale-125 hover:opacity-100"
+            className="block w-5 h-5 bg-white cursor-pointer opacity-50 z-10 transition-all duration-300 ease-in-out hover:scale-125 hover:opacity-100 border-2 border-black peer-checked/slider1:border-4 peer-checked/slider1:border-[#5D008F]"
             htmlFor="slider1"
           ></label>
           <label
-            className="block w-5 h-5 bg-white cursor-pointer opacity-50 z-10 transition-all duration-300 ease-in-out hover:scale-125 hover:opacity-100"
+            className="block w-5 h-5 bg-white cursor-pointer opacity-50 z-10 transition-all duration-300 ease-in-out hover:scale-125 hover:opacity-100 border-2 border-black peer-checked/slider2:border-4 peer-checked/slider2:border-[#5D008F]"
             htmlFor="slider2"
           ></label>
           <label
-            className="block w-5 h-5 bg-white cursor-pointer opacity-50 z-10 transition-all duration-300 ease-in-out hover:scale-125 hover:opacity-100"
+            className="block w-5 h-5 bg-white cursor-pointer opacity-50 z-10 transition-all duration-300 ease-in-out hover:scale-125 hover:opacity-100 border-2 border-black peer-checked/slider3:border-4 peer-checked/slider3:border-[#5D008F]"
             htmlFor="slider3"
           ></label>
         </div>
